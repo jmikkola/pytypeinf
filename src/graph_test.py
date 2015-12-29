40a1104016bcc2ec478c3299027079e79f740882
Jeremy Mikkola <jeremy@jeremymikkola.com>
2015-12-29 14:45:45 -0800

    Use nicer test method

--- src/graph_test.py
+++ src/graph_test.py
@@ -17,8 +17,8 @@ h: g d
 
 class GraphTest(unittest.TestCase):
     def test_parsing(self):
-        self.assertEqual(set('a b c d e f g h'.split()),
-                         set(test_graph.get_vertices()))
+        self.assertSetEqual(set('a b c d e f g h'.split()),
+                            set(test_graph.get_vertices()))
 
     def test_strongly_connected_components(self):
         scc = test_graph.strongly_connected_components()
